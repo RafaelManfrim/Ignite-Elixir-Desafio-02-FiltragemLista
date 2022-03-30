0995f89f95e24c317ea06d49add037a3d21a9c6f
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-30 20:22:34 -0300

    Desafio concluído

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,15 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
+  def call(list) do
+    Enum.map(list, fn x -> var_to_integer(x) end)
+    |> Enum.filter(fn x -> rem(x, 2) == 1 end)
+    |> Enum.count()
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp var_to_integer(x) do
+    try do
+      String.to_integer(x)
+    rescue
+      _e in ArgumentError -> 0
+    end
   end
 end
